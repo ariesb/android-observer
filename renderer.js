8f45e4981d7d1848a08d54acc59662dd1e823b22
Aries Beltran <abeltran@mssngsolutions.ph>
2016-07-28 18:11:10 +0800

    Initial version, working on Mac without Legacy support

---
+++ renderer.js
@@ -0,0 +1,285 @@
+/**
+ * android::observer
+ * @ariesbe, 2016
+ */
+const remote = require('electron').remote;
+
+var is_recording = false,
+  is_saving = false,
+  devices = {},
+  recorder = null;
+
+var every = require('every-moment');
+var timer = every(5, 'second', function() {
+  if (!is_recording) {
+    get_devices();
+  }
+});
+get_devices();
+
+
+function get_devices() {
+  console.log('Getting Devices');
+  const execFile = require('child_process').execFile;
+  const child = execFile('./adb', ['devices'], (error, stdout, stderr) => {
+    if (error) {
+      throw error;
+    }
+
+    const splitLines = require('split-lines');
+    var usb_devices = splitLines(stdout);
+    if (usb_devices.length > 3) {
+      console.log('Devices are connected.');
+      for (var i = 1, len = usb_devices.length - 2; i < len; i++) {
+        if (usb_devices[i] !== '') {
+          get_device_props(usb_devices[i].split('\t')[0]);
+        }
+      }
+    } else {
+      clear_device();
+    }
+
+  });
+}
+
+function get_device_props(deviceSerial) {
+  const execFile = require('child_process').execFile;
+  const child = execFile('./adb', ['-s', deviceSerial, 'shell', 'getprop'], (error, stdout, stderr) => {
+    if (error) {
+      throw error;
+    }
+    save_device_props(deviceSerial, stdout);
+  });
+}
+
+function save_device_props(deviceSerial, data) {
+  devices[deviceSerial] = {
+    manufacturer: '',
+    model: '',
+    androidVersion: ''
+  };
+  const splitLines = require('split-lines');
+  var getprops = splitLines(data);
+  for (var i = 0, len = getprops.length; i < len; i++) {
+    if (getprops[i].startsWith('[ro.product.manufacturer]')) {
+      // save manufacturer
+      devices[deviceSerial].manufacturer = getprops[i].split(':')[1].replace('[', '').replace(']', '').trim();
+    }
+
+    if (getprops[i].startsWith('[ro.product.model]')) {
+      // save manufacturer
+      devices[deviceSerial].model = getprops[i].split(':')[1].replace('[', '').replace(']', '').trim();
+    }
+
+    if (getprops[i].startsWith('[ro.build.version.release]')) {
+      // save manufacturer
+      devices[deviceSerial].androidVersion = getprops[i].split(':')[1].replace('[', '').replace(']', '').trim();
+    }
+  }
+
+  display_device_info(deviceSerial, devices[deviceSerial]);
+}
+
+function clear_device() {
+  document.getElementById('dev-serial').innerHTML = 'No connected device.';
+  document.getElementById('dev-manufacturer').innerHTML = '';
+  document.getElementById('dev-model').innerHTML = '';
+  document.getElementById('dev-android-version').innerHTML = '';
+  document.getElementById('no-device-connected').style.display = '';
+  document.getElementById('main-background').style.display = 'none';
+  document.getElementById('device-found').style.display = 'none';
+}
+
+function androidn(v) {
+  var vnames_upperlimit = {
+    '2.1': 'Donut',
+    '2.2': 'Eclair',
+    '2.3': 'Froyo',
+    '3.0': 'Gingerbread',
+    '4.0': 'Honeycomb',
+    '4.1': 'Ice Cream Sandwich',
+    '4.4': 'Jellybean',
+    '5.0': 'Kitkat',
+    '6.0': 'Lollipop',
+    '7.0': 'Marshmallow'
+  };
+  for (var key in vnames_upperlimit) {
+    if (v < key) {
+      return vnames_upperlimit[key];
+    }
+  }
+
+  return 'Unknown';
+}
+
+function display_device_info(deviceSerial, deviceInfo) {
+  document.getElementById('dev-serial').innerHTML = deviceSerial;
+  document.getElementById('dev-manufacturer').innerHTML = deviceInfo.manufacturer;
+  document.getElementById('dev-model').innerHTML = deviceInfo.model;
+  document.getElementById('dev-android-version').innerHTML = deviceInfo.androidVersion;
+  document.getElementById('dev-android-name').innerHTML = androidn(deviceInfo.androidVersion);
+  document.getElementById('no-device-connected').style.display = 'none';
+  document.getElementById('main-background').style.display = '';
+  document.getElementById('device-found').style.display = '';
+}
+
+function create_new_filename() {
+  return ('observer_' + (new Date().getTime()));
+}
+
+function record_device_activity(deviceSerial) {
+  console.log('Start recording from device.');
+  var filename = create_new_filename() + '.mp4';
+  document.getElementById('dev-record-file').innerHTML = filename;
+  const execFile = require('child_process').execFile;
+  is_recording = true;
+  recorder = execFile('./adb', ['-s', deviceSerial, 'shell', 'screenrecord', '--bit-rate', '6000000', '--verbose', '/sdcard/' + filename], (error, stdout, stderr) => {
+    if (error) {
+      // throw error;
+      console.log('Got error', error);
+    }
+  });
+  recorder.on('exit', function(code, signal) {
+    console.log('child process terminated due to receipt of signal ' + signal);
+    recorder = null;
+  });
+}
+
+function download_video_asc() {
+  const spawn = require('child_process').spawn;
+  var filename = document.getElementById('dev-record-file').innerHTML;
+  const ls = spawn('./adb', ['pull', '-p', '/sdcard/' + filename]);
+
+  ls.stderr.on('data', (data) => {
+    var percentage = data.toString().trim();
+    console.log(percentage, percentage.startsWith('Transferring'));
+    if( percentage.startsWith('Transferring') ) {
+      percentage = data.toString().split('(')[1].replace('%', '').replace(')', '');
+      console.log('stderr: ', percentage);
+      document.getElementById('rec-perc').innerHTML = percentage;
+    }
+  });
+
+  ls.on('close', (code) => {
+    console.log('child process exited with code', code);
+    setTimeout(function() {
+      console.log('Download completed.');
+      document.getElementById('rec-video').setAttribute('data-animation', '');
+      document.getElementById('rec-perc').innerHTML = '';
+      is_recording = is_saving = false;
+
+      delete_remote_recording(document.getElementById('dev-serial').innerHTML);
+
+      const vidfile = require('child_process').spawn;
+      const qt = vidfile('open', ['./' +  filename]);
+    }, 600);
+  });
+}
+
+function delete_remote_recording(deviceSerial) {
+  var filename = document.getElementById('dev-record-file').innerHTML;
+  const execFile = require('child_process').execFile;
+  var child = execFile('./adb', ['-s', deviceSerial, 'shell', 'rm', '-f', '/sdcard/' + filename], (error, stdout, stderr) => {
+    if (error) {
+      console.log('Error removing remote file.');
+      throw error;
+    }
+
+    console.log('Remove completed.');
+  });
+}
+
+function stop_recording(deviceSerial) {
+  recorder.kill();
+  is_recording = false;
+  setTimeout(function() {
+    download_video_asc();
+  }, 2000);
+}
+
+var screencap_movie = null, counter = 0;
+function record_device_activity_legacy(deviceSerial) {
+  var filename = create_new_filename();
+  document.getElementById('dev-record-file').innerHTML = filename;
+  is_recording = true;
+  recorder = {};
+  counter = 0;
+  screencap_movie = every(2, 'second', function() {
+    counter += 1;
+    var imagename = filename + '_' + ('00000'+counter).slice(-5) + '.png';
+    take_screencap(imagename);
+  });
+}
+
+function stop_recording_legacy(deviceSerial) {
+  recorder = null;
+  is_recording = false;
+  screencap_movie.stop();
+  setTimeout(function() {
+    download_video_asc_legacy();
+  }, 2000);
+}
+
+function download_video_asc_legacy() {
+  var filename = document.getElementById('dev-record-file').innerHTML;
+
+  for(var ii = 0; ii < counter; ii++) {
+    var percentage = Math.floor((ii / counter) * 100);
+    document.getElementById('rec-perc').innerHTML = percentage;
+    var imagename = filename + '_' + ('00000'+ii).slice(-5) + '.png';
+    pull_screencap(imagename);
+  }
+
+  setTimeout(function() {
+    console.log('Download completed.');
+    document.getElementById('rec-video').setAttribute('data-animation', '');
+    document.getElementById('rec-perc').innerHTML = '';
+    is_recording = is_saving = false;
+
+    // delete_remote_recording(document.getElementById('dev-serial').innerHTML);
+
+    // const vidfile = require('child_process').spawn;
+    // const qt = vidfile('open', ['./' +  filename]);
+  }, 600);
+}
+
+
+function take_screencap(filename) {
+  const execFile = require('child_process').execFile;
+  execFile('./adb', ['shell', 'screencap', '/sdcard/' + filename], (error, stdout, stderr) => {
+    if (error) {
+      console.log('Got error', error);
+    }
+  });
+}
+
+function pull_screencap(filename) {
+  const execFile = require('child_process').execFile;
+  execFile('./adb', ['pull', '/sdcard/' + filename, './screencaps/' + filename], (error, stdout, stderr) => {
+    if (error) {
+      console.log('Got error', error);
+    }
+  });
+}
+
+document.getElementById('rec-video').addEventListener('click', function(event) {
+  if (is_saving) return;
+
+  if (!is_recording && !is_saving) {
+    // start recording
+    this.setAttribute('data-animation', 'record');
+    record_device_activity_legacy(document.getElementById('dev-serial').innerHTML);
+    is_recording = true;
+  } else {
+    // start saving
+    this.setAttribute('data-animation', 'save');
+    document.getElementById('rec-perc').innerHTML = '0';
+    is_saving = true;
+    stop_recording_legacy(document.getElementById('dev-serial').innerHTML);
+  }
+});
+
+document.getElementById('close-app').addEventListener('click', function(event) {
+  var window = remote.getCurrentWindow();
+  window.close();
+});
